b0287e879caafa5506c6b4e6e1212cb83b087704
GabrielMuhlstedt <gabi.bapo@gmail.com>
2021-04-05 22:05:45 -0300

    Criacao comunicacao php com banco para cadastro

---
+++ js/funcoes.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+
+  fLocalEventosClick();
+
+
+});
+
+
+function fHashSenha(){
+
+  var senha_hash_md5 = $.MD5($('#senhaHash').val());
+  $("#senhaHash").val(senha_hash_md5);
+
+
+
+}
+
+
+function fLocalEventosClick(){
+  $("#bGravar").click(function(){
+
+    fHashSenha($("#senhaHash").val());
+    fLocalComunicaServidor("inserir");
+
+    return false;
+
+  });
+
+}
+
+function fLocalComunicaServidor(arquivo){
+
+  var valores = $("form").serialize();
+
+  $.ajax({
+
+    type: "POST",
+    dataType: "json",
+    data: valores,
+    url: "../php/" + arquivo + ".php",
+    success:function(retorno){
+
+    }
+  });
+
+}
